a1bce38717d68a51480f2645538423d870b7d8a5
Lidhin Antony <lidhin234@gmail.com>
2024-07-22 15:45:12 +0530

    Fully modified the styles. Added the darkmode change the color etc. Commiting the modified portfolio

--- script.js
+++ script.js
@@ -5,21 +5,54 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
-// Disable right-click
-document.addEventListener('contextmenu', (e) => e.preventDefault());
+// // Disable right-click
+// document.addEventListener('contextmenu', (e) => e.preventDefault());
 
-function ctrlShiftKey(e, keyCode) {
-  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
-}
+// function ctrlShiftKey(e, keyCode) {
+//   return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+// }
 
-document.onkeydown = (e,keyCode) => {
-  // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
-  if (
-    e.keyCode === 123 ||
-    ctrlShiftKey(e, 'I') ||
-    ctrlShiftKey(e, 'J') ||
-    ctrlShiftKey(e, 'C') ||
-    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
-  )
-    return false;
-};
+// document.onkeydown = (e,keyCode) => {
+//   // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+//   if (
+//     e.keyCode === 123 ||
+//     ctrlShiftKey(e, 'I') ||
+//     ctrlShiftKey(e, 'J') ||
+//     ctrlShiftKey(e, 'C') ||
+//     (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+//   )
+//     return false;
+// };
+
+
+const btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+  document.querySelector("header ul").classList.toggle("show");
+});
+
+const toTop = document.querySelector(".back-top");
+
+window.addEventListener("scroll", () => {
+  if (pageYOffset > 75) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
+
+
+
+
+let bars = document.querySelector(".bars");
+let menu = document.querySelector("#menu");
+
+bars.addEventListener("click", () => {
+  menu.classList.toggle("mobile-menu");
+});
+
+const sun = document.querySelector(".fa-sun");
+
+sun.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
+
